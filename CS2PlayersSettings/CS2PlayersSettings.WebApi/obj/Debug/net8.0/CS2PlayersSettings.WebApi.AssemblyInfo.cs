d971914fed1c36c7bfa553e2f285b0a5bd87a004
mianku <1960916590@qq.com>
2025-04-19 23:17:53 +0800

    Test

--- CS2PlayersSettings/CS2PlayersSettings.WebApi/obj/Debug/net8.0/CS2PlayersSettings.WebApi.AssemblyInfo.cs
+++ CS2PlayersSettings/CS2PlayersSettings.WebApi/obj/Debug/net8.0/CS2PlayersSettings.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS2PlayersSettings.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d78100b971be2d3af00156a5b3330eb699f107")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a33207da1ac10fe5b2acdb6e4743bc9a166ba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS2PlayersSettings.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS2PlayersSettings.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
